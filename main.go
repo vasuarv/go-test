652ba7789db0a085f19504b9473c736254a2bdf1
Arvind Vasuev <arvind.vasudev@riverbed.com>
2015-09-14 00:10:17 -0700

    Test2

--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 )
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "<h1>Hello from Cisco Shipped!</h1>\n")
+	fmt.Fprintf(w, "<h1>Hello from vasuarv on Cisco Shipped!</h1>\n")
 }
 
 func main() {
-    http.HandleFunc("/", defaultHandler)
-    fmt.Println("Example app listening at http://localhost:8888")
-    http.ListenAndServe(":8888", nil)
+	http.HandleFunc("/", defaultHandler)
+	fmt.Println("Example app listening at http://localhost:8888")
+	http.ListenAndServe(":8888", nil)
 }
